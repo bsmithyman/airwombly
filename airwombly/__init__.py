ecf29e5505344ff7d024d17b295103a45a06f1b5
Brendan Smithyman <brendan@bitsmithy.net>
2014-03-23 17:44:03 -0700

    Possibly pull static files and templates from the Git repository (if it already exists).

--- airwombly/__init__.py
+++ airwombly/__init__.py
@@ -1,9 +1,20 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
+# Pull Git repository configuration from environment
+gitkeys = ['LOCALREPO', 'REMOTEREPO']
+gitconfig = {key: os.getenv(key) for key in gitkeys}
+subdir = lambda x: '{0}/{1}'.format(gitconfig['LOCALREPO'], x)
 
-keys = ['LOCALREPO', 'REMOTEREPO']
-gitconfig = {key: os.getenv(key) for key in keys}
+
+# Check to see if static or template directories exist in the Git repo.
+# If so, substitute those for the ones hard coded into the app.
+kwargs = {}
+if os.path.isdir(subdir('static')):
+    kwargs['static_folder'] = gitstatic
+if os.path.isdir(subdir('templates')):
+    kwargs['template_folder'] = gittemplates
+
+app = Flask(__name__, **kwargs)
 
 from airwombly import views
